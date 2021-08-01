d6deea9bb4c5237eb7ec3b475b376d18eb50750a
Megg44 <76537255+Megg44@users.noreply.github.com>
2021-08-01 20:50:23 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -1,9 +1,9 @@
 import math
 s = input("Enter length, breadth and height.")
-my_list = list(map(int, s.split()))
+my_list = list(map(float, s.split()))
 
-volume = (my_list[0] ** 2)*(my_list[1] ** 2)/ sum(my_list)
+volume = (my_list[2] ** 2)*(my_list[1] ** 2)/ math.sqrt((my_list[0] ** 2) + (my_list[1] ** 2) + (my_list[2] ** 2)) 
 
 r = ((3 * volume) / (4 * math.pi)) ** (1/3)
 
-print(f"The volume of the tromboloid is {volume} and the radius of the sphere with the same volume would be {r}.")
+print(f"The volume of the tromboloid is {volume:.3f} and the radius of the sphere with the same volume would be {r:.3f}.")
