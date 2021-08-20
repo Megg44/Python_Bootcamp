2d52cf43e4bdec6da021ef626e6781c7dcaba1f5
Megg44 <76537255+Megg44@users.noreply.github.com>
2021-08-20 23:41:46 +0530

    Update Activity_18.py

--- Activity_18.py
+++ Activity_18.py
@@ -1,16 +1,16 @@
 def stringin():
     return input("Enter your string.")
 
-def stringtolist(s):
+def stringtodict(s):
     my_list = list(map(lambda x : x.partition("="), s.split(";")))
     return dict([(x, z) for (x, y, z) in my_list])
 
-def printlist(list_of_list):
-    print(list_of_list)
+def printdict(dictionary):
+    print(dictionary)
 
 def main():
     string = stringin()
-    my_list = stringtolist(string)
-    printlist(my_list)
+    dictionary = stringtodict(string)
+    printlist(dictionary)
 
 main()
