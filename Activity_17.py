8614389deed74bc5f7da1a80a09161e6e3622028
Megg44 <76537255+Megg44@users.noreply.github.com>
2021-08-20 22:46:20 +0530

    love u jeev

---
+++ Activity_17.py
@@ -0,0 +1,26 @@
+def stringin():
+    return input("Enter your string.")
+
+def stringtolist(s):
+    my_list = list(map(lambda x : x.partition("="), s.split(";")))
+    return [(x, z) for (x, y, z) in my_list]
+
+def listtostring(my_list):
+    newList = ["=".join(e) for e in my_list]
+    return ";".join(map(str, newList))
+    
+    
+def printlist(list_of_list):
+    print(list_of_list)
+
+def printstring(string):
+    print(string)
+
+def main():
+    string = stringin()
+    my_list = stringtolist(string)
+    string2 = listtostring(my_list)
+    printlist(my_list)
+    printstring(string)
+
+main()
